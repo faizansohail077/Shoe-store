44510a1f66cff31ef82af024e532b5af9fa4bce6
Muhammad Faizan <faizansohail077@gmail.com>
2020-11-12 01:23:52 -0500

    working on it

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,34 +1,20 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import './ProductDetails.css'
+import { shoes } from './Global/Globalstate'
 function ProductDetails() {
-    const shoes = {
-        "air-jordan-3-valor-blue": {
-            name: "VALOUR BLUE",
-            img:
-                "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-        },
-        "jordan-mars-270-london": {
-            name: "JORDAN MARS 270 LONDON",
-            img:
-                "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-        },
-        "air-jordan-1-zoom-racer-blue": {
-            name: "RACER BLUE",
-            img:
-                "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-        }
-    };
-    const {id}=useParams()
-    const product=shoes[id]
-    const {name,img}=product
+
+    const { id } = useParams()
+    const product = shoes[id]
+    const { name, img, price } = product
     return (
         <div>
             ProductDetails
             <div>
                 {/* {id} */}
                 <h2>{name}</h2>
-                <img src={img} alt=''   />
+                <h1>${price}</h1>
+                <img src={img} alt='' />
             </div>
         </div>
     )
--- src/Components/ProductIndex.jsx
+++ src/Components/ProductIndex.jsx
@@ -1,32 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import { shoes } from './Global/Globalstate'
 
 function ProductIndex() {
-    const shoes = {
-        "air-jordan-3-valor-blue": {
-            name: "VALOUR BLUE",
-            img:
-                "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-        },
-        "jordan-mars-270-london": {
-            name: "JORDAN MARS 270 LONDON",
-            img:
-                "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-        },
-        "air-jordan-1-zoom-racer-blue": {
-            name: "RACER BLUE",
-            img:
-                "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-        }
-    };
     return (
         <div>
-              <ul>
-                {Object.entries(shoes).map(([productID, { name, img }]) => (
+            <ul>
+                {Object.entries(shoes).map(([productID, { name, img, price }]) => (
                     <li key={productID}>
                         <Link to={productID}>
-                            <h2>{name}</h2>
-                            <img src={img} alt={name} />
+                            <div>
+                                <h2>{name}</h2>
+                                <img src={img} alt={name} />
+                                <h1>{price}</h1>
+                            </div>
                         </Link>
                     </li>
                 ))}
